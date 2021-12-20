a8d0bd2cbd031d9e628c6bf54dcca98c802a3f5f
Emmett He <emmetthe26@gmail.com>
2021-12-20 10:48:02 -0800

    push

--- frontend/emmtsy.jsx
+++ frontend/emmtsy.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Root from './component/root';
 import configureStore from './store/store';
 
-import * as Cart from './action/cart_action';
+// import * as Cart from './action/cart_action';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -23,12 +23,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
   // tests------------------
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.fetchCartItems = Cart.fetchCartItems;
-  window.createCart = Cart.createCart;
-  window.deleteCart = Cart.deleteCartItem;
-  window.updateCart = Cart.updateCartItem;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.fetchCartItems = Cart.fetchCartItems;
+  // window.createCart = Cart.createCart;
+  // window.deleteCart = Cart.deleteCartItem;
+  // window.updateCart = Cart.updateCartItem;
   // ---------------------------
   ReactDOM.render(<Root store={store} />, root);
 });
